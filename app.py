c74200454d45d084dcd5404523f94cebbd95e703
umbilnm <kill4ik2002@gmail.com>
2024-05-13 03:22:42 +0300

    Refactor code

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from model import inference_cv_model
 import time
 st.title("Classifier")
 file = st.file_uploader("Загрузите файл", type=["png", "jpg"])
-
+st.write(f'Данное приложение классифицирует фотографии на три класса: анкета из дейтинг приложения, фотография, скрин переписки')
+    
 
 if file is not None:
     start = time.time()
-    st.write(f'Данное приложение классифицирует фотографии на три класса: анкета из дейтинг приложения, фотография, скрин переписки')
     st.write("Вы загрузили файл:", file.name)
     data = file.getvalue()
     label = inference_cv_model(data)
