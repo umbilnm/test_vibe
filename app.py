93445d9b6ae76620985b5b889ef11d065a888073
umbilnm <kill4ik2002@gmail.com>
2024-05-13 03:21:01 +0300

    Update README

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ file = st.file_uploader("Загрузите файл", type=["png", "jpg"])
 
 if file is not None:
     start = time.time()
+    st.write(f'Данное приложение классифицирует фотографии на три класса: анкета из дейтинг приложения, фотография, скрин переписки')
     st.write("Вы загрузили файл:", file.name)
     data = file.getvalue()
     label = inference_cv_model(data)
